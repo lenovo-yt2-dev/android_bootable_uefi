c3bd4e7a916e4191e0301e04508551f01cd57742
Sylvain Chouleur <sylvain.chouleur@intel.com>
2013-10-09 10:33:35 +0200

    Boot logic implementation
    BZ: 143502
    
    Add missing file platform/x86.c
    
    Change-Id: If93daba441e8ab7dabed62c644ada4f3cfe82c6c
    Signed-off-by: Sylvain Chouleur <sylvain.chouleur@intel.com>
    Reviewed-on: http://android.intel.com:8080/137140

---
+++ efilinux/platform/x86.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer
+ *      in the documentation and/or other materials provided with the
+ *      distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <efi.h>
+#include "platform.h"
+#include "intel_partitions.h"
+#include "acpi.h"
+#include "uefi_osnib.h"
+
+void x86_ops(struct osloader_ops *ops)
+{
+	ops->check_partition_table = check_gpt;
+	ops->read_flow_type = acpi_read_flow_type;
+	ops->do_cold_off = acpi_cold_off;
+	ops->populate_indicators = rsci_populate_indicators;
+	ops->load_target = intel_load_target;
+	ops->get_wake_source = rsci_get_wake_source;
+	ops->get_reset_source = rsci_get_reset_source;
+	ops->get_shutdown_source = rsci_get_shutdown_source;
+	ops->is_osnib_corrupted = uefi_is_osnib_corrupted;
+	ops->reset_osnib = uefi_reset_osnib;
+}
